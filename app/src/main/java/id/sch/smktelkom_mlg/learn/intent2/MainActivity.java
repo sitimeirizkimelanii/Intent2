8282391a6b5181f3665a384fb5be563e450c9942
sitimeirizkimelanii <siti_melani_23rpl@student.smktelkom-mlg.sch.id>
2016-11-07 22:28:16 +0700

    Initial Commit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/MainActivity.java
@@ -1,13 +1,8 @@
 package id.sch.smktelkom_mlg.learn.intent2;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
